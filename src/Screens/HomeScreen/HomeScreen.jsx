f0b24879a82ba410c25b840b4eefb612ade597d1
Yamila_Perez <yamilaperez.dev@gmail.com>
2025-11-27 01:42:02 -0300

    Agregué HomeScreen - sin funcionalidad

--- src/Screens/HomeScreen/HomeScreen.jsx
+++ src/Screens/HomeScreen/HomeScreen.jsx
@@ -1,9 +1,19 @@
 import React from "react";
+import '../../styles/HomeScreen.css'
+import { useContext } from "react";
+import { AuthContext } from "../../Context/AuthContext";
 
 const HomeScreen = () => {
+    const { user } = useContext(AuthContext);
+
 return (
-    <div>
-        <p>Inicio</p>
+    <div className="container__home">
+        <div className="container__form">
+        <h1 className="home__title">¡Hola, {user?.name || "usuario"}!</h1>
+        <span>¿Qué deseas hacer hoy?</span>
+        <button className="button__form">Modificar una reserva</button>
+        <button className="button__form">Reservar un nuevo turno</button>
+        </div>
     </div>
 )
 }
